0acd0207c05d3b2e825c5fedd4e80c1eaedd0dd9
rezojoglidze <rezojoglidze7@gmail.com>
2020-06-28 16:58:13 +0400

    added LogIn, Registration Modules

---
+++ Auth/Registration.php
@@ -0,0 +1,39 @@
+<?php
+
+$server = "localhost";
+$usernm = "root";
+$password = "";
+$dbname = "webProject";
+
+//$con = mysqli_connect($server, $usernm, $passwd,$dbname);
+
+$con = new mysqli($server, $usernm, $passwd, $dbname);
+
+if($con->connect_error){
+	die("Connection failed " . $con->connect_error);
+}
+
+$id = 5;
+$email = stripcslashes($_POST["email"]);
+$email = mysqli_real_escape_string($con,$email);
+
+$password = stripcslashes($_POST["psw"]);
+$password = mysqli_real_escape_string($con,$password);
+
+
+
+$sql = $con->prepare(" INSERT INTO registration(id,email,password) VALUES(?,?,?) ");
+
+$sql->bind_param("isssi", $id, $email, $password);
+
+if($sql->execute()){
+	echo "record added";
+}
+
+else
+{
+	echo "something went wrong " . $con->mysqli_error;
+}
+
+
+?>
