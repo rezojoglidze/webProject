018a6327481caa8edf91990d418d06a3dbb9ea44
rezojoglidze <rezojoglidze7@gmail.com>
2020-07-03 14:18:12 +0400

    added show user details logic

--- AdminPanel/users.php
+++ AdminPanel/users.php
@@ -24,36 +24,42 @@
 <div id="gallery-content">
   <div class="gallery" style= "width: 80%; margin: 20px auto; border: 1px solid #cbcbcb; padding: 50px">
 
+ <form>
+     <button style="border-radius: 25px" onclick="getUserDetails()" class="registerbtn">იუზერის დეტალები</button>
+</form>
+
       <form action="delete.php" method="post">
       <p>
-         <select name="userSelect"; style="margin-bottom: 5px"; id="select1">
+         <select name="userSelect"; onChange="myNewFunction(this.selectedIndex);"  style="margin-bottom: 5px"; id="select1">
            <?php while($row = $result->fetch_assoc()){ ?>
-            <option value="<?php echo $row['id'] ?>"><?php echo $row['firstName']; echo "   "; echo $row['lastName'] ?> </option>
+            <option value="<?php echo $row['id']?>"><?php echo $row['firstName']; echo "   "; echo $row['lastName'] ?> </option>
            <?php } ?> </select>
-          <input type="submit" name="submit" value="Get Selected Values" />
          </p>
-       <p style="margin-bottom: 5px";> The value of the option selected is: <span class="output"></span> </p>
-
-            <button name="deleteBtnTapped" onclick="getOption()" class="registerbtn">წაშლა</button>
-            <button name="updateBtnTapped" onclick="getOption()" class="registerbtn">განახლება</button>
+               <p style="margin-bottom: 5px";> მომხარებლის id: <span class="id"></span> </p>
+               <p style="margin-bottom: 5px";> სახელი და გვარი: <span class="fullName"></span> </p>
+            <button style="border-radius: 25px" name="deleteBtnTapped" class="registerbtn">წაშლა</button>
+            <button style="border-radius: 25px" name="updateBtnTapped" class="registerbtn">განახლება</button>
        </form>
    </div>
 </div>
 
            <!-- /* get userid from choosen user */ -->
            <?php
-             if(isset($_POST['deleteBtnTapped']) || isset($_POST['submit'])){
-             $selected_val = $_POST['userSelect'];  // Storing Selected Value In Variable
-             echo "You have selected : $selected_val";  // Displaying Selected Value
+             if(isset($_POST['deleteBtnTapped'])){
+             $selected_val = $_POST['userSelect'];
+             echo "You have selected : $selected_val";
              $_SESSION['selectedid'] = $_POST['userSelect'];
              }
              ?>
 
     <script type="text/javascript">
-        function getOption() {
+
+         function getUserDetails() {
             selectElement = document.querySelector('#select1');
             output = selectElement.value;
-            document.querySelector('.output').textContent = output;
+            fullName = document.getElementById("select1");
+            document.querySelector('.id').textContent = output;
+            document.querySelector('.fullName').textContent = fullName.options[fullName.selectedIndex].text;
         }
     </script>
 </body>
